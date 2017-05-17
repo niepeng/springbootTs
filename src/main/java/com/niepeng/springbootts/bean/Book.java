bc59bec7f1b732ebb64522f8c7b3f37011774ba1
niepeng <niepeng>
2017-05-17 20:15:02 +0800

    add controller for restful and properties test.

---
+++ src/main/java/com/niepeng/springbootts/bean/Book.java
@@ -0,0 +1,23 @@
+package com.niepeng.springbootts.bean;
+/**
+ * Created by lsb on 17/5/17.
+ */
+
+
+import lombok.Data;
+
+/**
+ * @author 聂鹏
+ * @version 1.0
+ * @date 17/5/17
+ */
+@Data
+public class Book {
+
+  private Long id;
+
+  private String name;
+
+  private Integer age;
+
+}
